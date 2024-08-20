c91866719e9249aa5df1ac49179a2c93e53bf809
benjypng <47936793+benjypng@users.noreply.github.com>
2024-08-20 11:42:46 +0800

    fix: regex

--- src/features/one-way-sync/index.ts
+++ src/features/one-way-sync/index.ts
@@ -2,11 +2,8 @@ import { insertTask } from '../../services/insert-task'
 
 export const oneWaySync = () => {
   logseq.DB.onChanged(async ({ blocks }) => {
-    if (
-      blocks.length === 2 &&
-      blocks[0] &&
-      /#\[?\[?\w+\]?\]?/.test(blocks[0].content)
-    ) {
+    const re = new RegExp(`#[?[?${logseq.settings!.oneWayTag}]?]?`, 'g')
+    if (blocks.length === 2 && blocks[0] && re.test(blocks[0].content)) {
       await insertTask(blocks[0])
     }
   })
