3462d53a146819434491f74d2a8215a382acc7b4
benjypng <47936793+benjypng@users.noreply.github.com>
2024-08-21 14:14:15 +0800

    fix: regex

--- src/features/one-way-sync/index.ts
+++ src/features/one-way-sync/index.ts
@@ -1,10 +1,36 @@
 import { insertTask } from '../../services/insert-task'
 
 export const oneWaySync = () => {
-  logseq.DB.onChanged(async ({ blocks }) => {
-    const re = new RegExp(`#[?[?${logseq.settings!.oneWayTag}]?]?`, 'g')
-    if (blocks.length === 2 && blocks[0] && re.test(blocks[0].content)) {
-      await insertTask(blocks[0])
+  // Using DB.onChanged is unreliable for this purpose as it triggers the API even for typos
+  const re = new RegExp(
+    `#(?:\\[\\[)?(${logseq.settings!.oneWayTag})(?:\\]\\])?`,
+    'i',
+  )
+
+  const callback = async (mutationsList: MutationRecord[]): Promise<void> => {
+    for (const m of mutationsList) {
+      if (
+        m.type === 'childList' &&
+        m.removedNodes.length > 0 &&
+        (m.removedNodes[0]! as HTMLElement).className ===
+          'editor-inner block-editor'
+      ) {
+        const uuid = (m.target as HTMLElement)
+          .closest('div[id^="ls-block"]')
+          ?.getAttribute('blockid') as string
+        const currBlock = await logseq.Editor.getBlock(uuid)
+        if (!currBlock) return
+
+        if (re.test(currBlock.content)) await insertTask(currBlock)
+      }
     }
+  }
+
+  //@ts-expect-error Mutation does not exist on window
+  const observer = new parent.MutationObserver(callback)
+  observer.observe(parent.document.getElementById('app-container'), {
+    attributes: false,
+    childList: true,
+    subtree: true,
   })
 }
